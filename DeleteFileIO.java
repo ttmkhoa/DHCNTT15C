9f29e4cd9e34c2b1cbd641bbb1734387702d4a4b
Khoa Tran <ttmk84@gmail.com>
2021-09-24 13:56:14 +0700

    cKhoa them dong 5 tu vsc

--- DeleteFileIO.java
+++ DeleteFileIO.java
@@ -2,43 +2,39 @@ package example;
 
 import java.io.File;
 
+//them noi dung tu vsc
 public class DeleteFileIO {
 	private void deleteFile(String source) {
-		//new file
+		// new file
 		File file = new File(source);
-		//check file exist
+		// check file exist
 		// neu ton tai
-		if(file.exists()) {
+		if (file.exists()) {
 			System.out.println("file ton tai");
 			file.delete();
 			System.out.print("xoa file thanh cong");
-		}
-		else {
+		} else {
 			System.out.println("file khong ton tai");
 		}
 	}
-//them ghi chu dong 20	
-	public boolean deleteEmptyFolder(String source)
-	{
+
+	// them ghi chu dong 20
+	public boolean deleteEmptyFolder(String source) {
 		File folder = new File(source);
-		//kiem tra neu folder ton tai thi xoa
-		if(folder.exists())
-		{
+		// kiem tra neu folder ton tai thi xoa
+		if (folder.exists()) {
 			folder.delete();
 			System.out.println("folder ton tai\n xoa folder thanh cong");
 			return true;
-		}
-		else
-		{
+		} else {
 			System.out.println("folder khong ton tai");
 		}
-		return false;		
+		return false;
 	}
-	
-	
+
 	public static void main(String[] args) {
 		DeleteFileIO deleteFileIO = new DeleteFileIO();
-		//deleteFileIO.deleteFile("D:/HocJava/demo.txt");
+		// deleteFileIO.deleteFile("D:/HocJava/demo.txt");
 		deleteFileIO.deleteEmptyFolder("D:/HocJava/testFolder");
 	}
 }
